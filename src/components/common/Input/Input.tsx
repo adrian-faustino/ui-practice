1757750af91287d564463318860fa8e0a6b2d65a
Adrian Faustino <contact.adrianfausitno@gmail.com>
2022-02-18 21:52:17 -0700

    Fix maxLength issue not allowing select all delete on Input component

--- src/components/common/Input/Input.tsx
+++ src/components/common/Input/Input.tsx
@@ -2,8 +2,7 @@ import React from "react";
 import classNames from "classnames";
 import Button from "components/common/Button";
 import CrossIcon from "icons/CrossIcon";
-import { IUseInput, IInputChangeEvent, IInputKeyDownEvent } from "types/global";
-// import { emptyFn } from "constants/global";
+import { IUseInput } from "types/global";
 
 import "./Input.scss";
 
@@ -28,27 +27,6 @@ const Input: React.FC<IInputProps> = ({
 }) => {
   const _classNames = classNames("Input", className);
 
-  // returns function to update state if maxLength requirements are met
-  const onChange = (e: IInputChangeEvent) => {
-    if (maxLength && inputState.values[name]?.length >= maxLength) {
-      return;
-    }
-
-    inputState.onHandleChange(e);
-  };
-
-  const onKeyDown = (e: IInputKeyDownEvent) => {
-    // handling delete here fixes issue where once input length has reached maxLength, early return prevents further state change (in onChange fn)
-    if (e.key === "Backspace") {
-      e.preventDefault(); // stops propagation to onChange handler
-
-      inputState.setValues((state = {}) => ({
-        ...state,
-        [name]: state[name]?.slice(0, -1),
-      }));
-    }
-  };
-
   return (
     <div className={_classNames}>
       <input
@@ -56,9 +34,9 @@ const Input: React.FC<IInputProps> = ({
         className="Input__field"
         name={name}
         placeholder={placeholder}
+        maxLength={maxLength}
         value={inputState.values[name] || ""}
-        onKeyDown={onKeyDown}
-        onChange={onChange}
+        onChange={inputState.onHandleChange}
       />
 
       <Button
